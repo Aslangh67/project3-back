4127ed146bfc28a8d66d2008d9da8e2e7c2a3c22
Arauco <arishel@gmail.com>
2020-02-13 16:07:45 -0800

    fixed put routes on all controllers

--- controllers/companyProfile.js
+++ controllers/companyProfile.js
@@ -8,17 +8,17 @@ module.exports = {
   // Create company item
   // api/company/new
   newCompany: function (req, res) {
+    console.log(req.body);
+
     db.Company_profile.create({
       company_name: req.body.company_name,
-      // delete main address after syncing with Aslan's changes
-      main_address: req.body.main_address,
       ein: req.body.ein,
-      account_type: req.body.account_type,
-      ein: req.body.ein
+      account_type: req.body.account_type
     })
       .then(function (data) {
         // return data
         res.json(data);
+        // console.log(res.json(data));
       }).catch(function (err) {
         console.error(err);
       })
@@ -26,7 +26,7 @@ module.exports = {
 
   // Get all companies
   // api/company/
-  getCompany: function (req, res) {
+  getCompanies: function (req, res) {
     const query = {};
     db.Company_profile.findAll({
       where: query
@@ -53,12 +53,11 @@ module.exports = {
   editCompany: function (req, res) {
     db.Company_profile.update(
       req.body, {
+      company_name: req.body.company_name,
+      ein: req.body.ein,
+      account_type: req.body.account_type,
       where: {
-        id: req.body.id,
-        // I'm not sure about these
-        date: req.body.date,
-        Company_profile_id: req.body.Company_profile_id,
-        Location_id: req.body.Location_id
+        id: req.params.id,
       }
     }).then(function (dbCompany) {
       res.json(dbCompany);
